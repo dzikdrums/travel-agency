419a0e528eef9e83df93cd02660c7fb7b9f6f1b7
user <user@users-MacBook-Air.local>
2019-10-30 15:37:33 +0100

    fix filtering trips

---
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -0,0 +1,28 @@
+import { connect } from 'react-redux';
+import TripListOptions from './TripListOptions';
+import { getAllTags } from '../../../redux/tagsRedux';
+import {
+  getAllFilters,
+  changeSearchPhrase,
+  addTags,
+  removeTags,
+  changeDuration,
+} from '../../../redux/filtersRedux';
+
+const mapStateToProps = state => ({
+  tags: getAllTags(state),
+  filters: getAllFilters(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
+  // TODO - add more dispatchers for other filters
+  addTags: tag => dispatch(addTags(tag)),
+  removeTags: tag => dispatch(removeTags(tag)),
+  changeDuration: (type, value) => dispatch(changeDuration(type, value)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(TripListOptions);
