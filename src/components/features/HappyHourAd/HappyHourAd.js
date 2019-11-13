2e39e085ec77e3f110da1343adb87574d86a1d26
user <user@users-MacBook-Air.local>
2019-11-13 14:53:47 +0100

    add tests to HappyHourAd component

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,14 +1,50 @@
 import React from 'react';
-import styles from './HappyHour.scss';
+import styles from './HappyHourAd.scss';
 import PropTypes from 'prop-types';
 
 class HappyHourAd extends React.Component {
+  constructor() {
+    super();
+    setInterval(() => {
+      this.forceUpdate();
+    }, 1000);
+  }
+
+  mockProps = {
+    title: 'HappyHour',
+    promoDescription: 'promoDescription',
+  };
+
+  getCountdownTime() {
+    const currentTime = new Date();
+    const nextNoon = new Date(
+      Date.UTC(
+        currentTime.getUTCFullYear(),
+        currentTime.getUTCMonth(),
+        currentTime.getUTCDate(),
+        12,
+        0,
+        0,
+        0,
+      ),
+    );
+
+    if (currentTime.getUTCHours() >= 12) {
+      nextNoon.setUTCDate(currentTime.getUTCDate() + 1);
+    }
+
+    return Math.round((nextNoon.getTime() - currentTime.getTime()) / 1000);
+  }
+
   render() {
     return (
-      <div>
-        <h3 className={styles.title}>{this.props.title}</h3>
-        <p className={styles.promoDescription}></p>
-        <div className={styles.countdown}></div>
+      <div className={styles.component}>
+        <h3 className={styles.title}>{this.mockProps.title}</h3>
+        <div className={styles.promoDescription}>
+          {this.getCountdownTime() <= 82800
+            ? this.getCountdownTime()
+            : this.mockProps.promoDescription}
+        </div>
       </div>
     );
   }
@@ -16,6 +52,7 @@ class HappyHourAd extends React.Component {
 
 HappyHourAd.propTypes = {
   title: PropTypes.string,
+  promoDescription: PropTypes.string,
 };
 
 export default HappyHourAd;
--- src/components/layout/Hero/Hero.test.js
+++ src/components/layout/Hero/Hero.test.js
@@ -22,4 +22,13 @@ describe('Component Hero', () => {
     expect(renderedTitle).toEqual(expectedTitle);
     expect(component.find('.image').prop('src')).toEqual(expectedImage);
   });
+  it('should render HappyHourAd', () => {
+    const expectedTitle = 'Lorem ipsum';
+    const expectedImage = 'image.jpg';
+    const component = shallow(
+      <Hero titleText={expectedTitle} imageSrc={expectedImage} />,
+    );
+
+    expect(component.find('HappyHourAd').length).toEqual(1);
+  });
 });
